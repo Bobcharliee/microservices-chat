1c33cac3f8464f07bb87c6549fe13d0e6a2f1ac8
Bobcharliee <anayocharles18@gmail.com>
2026-08-10 13:14:20 -0400

    editted provider config

--- Terraform/root/provider.tf
+++ Terraform/root/provider.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
@@ -15,6 +14,5 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  skip_signature_validation = true
 }
 
